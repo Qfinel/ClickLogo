908c8f849f7f6c7646e02a48adf25df429bff51d
Jan Tsizik <jtsizik@3-B-10.42heilbronn.de>
2023-06-09 20:14:28 +0200

    Added scroll arrow and section with services

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -5,8 +5,35 @@ import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import pic from '../assets/Man.jpg'
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
 
 const Home = () => {
+
+	const navigate = useNavigate();
+
+	const [isVisible, setIsVisible] = useState(false);
+
+	const handleScroll = () => {
+		const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		setIsVisible(scrollTop > 100); // Show the button when scrolled 100 pixels down
+	};
+
+	const scrollToTop = () => {
+		window.scrollTo({
+		top: 0,
+		behavior: 'smooth',
+		});
+	};
+
+	useEffect(() => {
+		window.addEventListener('scroll', handleScroll);
+		return () => {
+		window.removeEventListener('scroll', handleScroll);
+		};
+	}, []);
+
 	return (
 		<div className={styles.HomeCont}>
 			<Header />
@@ -25,7 +52,23 @@ const Home = () => {
 			</section>
 					<img src={pic} className={styles.pic} alt=""/>
 			</section>
+			<section className={styles.ServiceSection}>
+				<h1>Wide <span style={{color: '#e43256'}}>variety</span> of services</h1>
+				<section>
+					<div className={`${styles.ServiceCard} ${styles.Wordmark}`} onClick={() => {navigate('/services')}}><p>Wordmarks</p></div>
+					<div className={`${styles.ServiceCard} ${styles.Letterform}`} onClick={() => {navigate('/services')}}><p>Letterforms</p></div>
+					<div className={`${styles.ServiceCard} ${styles.Lettermark}`} onClick={() => {navigate('/services')}}><p>Lettermarks</p></div>
+					<div className={`${styles.ServiceCard} ${styles.LogoSymb}`} onClick={() => {navigate('/services')}}><p>Logo symbols</p></div>
+					<div className={`${styles.ServiceCard} ${styles.AbstractLogo}`} onClick={() => {navigate('/services')}}><p>Abstract logo</p></div>
+					<div className={`${styles.ServiceCard} ${styles.Mascot}`} onClick={() => {navigate('/services')}}><p>Mascots</p></div>
+				</section>
+			</section>
 			<Footer />
+			<button
+				className={`${styles.scrollBtn} ${isVisible ? '' : styles.invisible}`}
+				onClick={scrollToTop}>
+				<FontAwesomeIcon icon={faArrowUp} color="#0e0d17"/>
+			</button>
 		</div>
 	);
 }
